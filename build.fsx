e761646c58260db5b8906c8c4d228a9013906f23
wraikny <writtenknown@gmail.com>
2021-01-27 17:54:06 +0900

    fix

--- build.fsx
+++ build.fsx
@@ -1,5 +1,6 @@
 #r "paket:
 source https://api.nuget.org/v3/index.json
+nuget FSharp.Core 4.7.0.0
 nuget Fake.DotNet.Cli
 nuget Fake.IO.FileSystem
 nuget Fake.Core.Target
@@ -13,9 +14,7 @@ nuget FSharp.Json //"
 open System
 open Fake.Core
 open Fake.DotNet
-open Fake.DotNet.Testing
 open Fake.IO
-open Fake.IO.FileSystemOperators
 open Fake.IO.Globbing.Operators
 open Fake.Core.TargetOperators
 open Fake.Net
@@ -38,17 +37,6 @@ let dotnet cmd arg =
     failwithf "Failed 'dotnet %s %s'" cmd arg
 
 Target.initEnvironment()
-
-Target.create "Test" (fun _ ->
-  [ for x in testProjects ->
-      sprintf "tests/%s/bin/Release/**/%s.dll" x x
-  ] |> function
-  | [] ->
-    printfn "There is no test project"
-  | x::xs ->
-    Seq.fold (++) (!! x) xs
-    |> Expecto.run id
-)
 
 Target.create "Tool" (fun _ ->
   dotnet "tool" "update fake-cli"
